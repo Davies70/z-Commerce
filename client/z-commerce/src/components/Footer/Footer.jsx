f772431eadfbd55a2e823c8881b8a1d44d28dc25
Ajayi Davies <ajayidavies7@gmail.com>
2023-04-20 13:21:03 +0200

    header section completed

--- client/z-commerce/src/components/Footer/Footer.jsx
+++ client/z-commerce/src/components/Footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className='footer'>
+    <div className="top">
+      <div className="item"></div>
+      <h1>Categories</h1>
+      <span>Women</span>
+      <span>Men</span>
+      <span>Shoes</span>
+      <span>Accessories</span>
+      <span>New Arrivals</span>
+    </div>
+    <div className="bottom"></div>
+  </div>;
 };
 
 export default Footer;
