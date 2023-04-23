7bbe604fe9a97c8dfa2260cf971c08e2352d6fac
Ajayi Davies <ajayidavies7@gmail.com>
2023-04-23 15:17:22 +0200

    Slider implemented with useState hook

---
+++ client/z-commerce/src/components/Slider/Slider.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import EastOutlinedIcon from '@mui/icons-material/EastOutlined';
+import WestOutlinedIcon from '@mui/icons-material/WestOutlined';
+import './Slider.scss';
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (next) => next + 1);
+  };
+
+  const data = [
+    'https://images.pexels.com/photos/965324/pexels-photo-965324.jpeg',
+    'https://images.pexels.com/photos/9717986/pexels-photo-9717986.jpeg',
+    'https://images.pexels.com/photos/9637586/pexels-photo-9637586.jpeg',
+  ];
+  return (
+    <div className="slider">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <WestOutlinedIcon />
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <EastOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
